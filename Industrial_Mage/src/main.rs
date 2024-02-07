9ea7013a1545bc83c937906324958bd3eda42448
drone-spec <drone@dronemothership.work>
2024-02-07 16:23:33 -0600

    Okay added in the Tileset but Display keeps crashing due to Arch Distro and Displayport issue. :( RIP

--- Industrial_Mage/src/main.rs
+++ Industrial_Mage/src/main.rs
@@ -21,83 +21,7 @@ use camera::CameraLogic;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-<<<<<<< HEAD
-        .add_systems(Startup, setup)
-        .add_systems(Startup, spawn_wizard)
-        .add_systems(Update, sprite_movement)
-        .run();
-}
-
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("goblinhole/goblin.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    ));
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("goblinhole/goblin.png"),
-            transform: Transform::from_xyz(150., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    ));
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("goblinhole/goblin.png"),
-            transform: Transform::from_xyz(80., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    ));
-
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("goblinhole/goblinwore.png"),
-            transform: Transform::from_xyz(380., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    ));
-}
-
-fn spawn_wizard (mut commands: Commands, asset_server: Res<AssetServer>){
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("wiznerd/Wizard.png"),
-            transform: Transform::from_xyz(-380., 0., 0.),
-            ..default()
-        },
-        Direction::Down,
-    ));
-}
-
-/// The sprite is animated by changing its translation depending on the time that has passed since
-/// the last frame.
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
-    }
-}
-=======
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(WizardPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(MovementPlugin)
@@ -106,4 +30,3 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
         .add_systems(Update, close_on_esc)
         .run();
 }
->>>>>>> e64e86eb5714a8069b218536d4bf4305185f8234
--- Industrial_Mage/src/ore.rs
+++ Industrial_Mage/src/ore.rs
@@ -1,17 +1,39 @@
 // This should hold all Logic to do with Ore and Maybe all functions associated with those systems
 // Also World Generation!!!
-use bevy::prelude::*;
+use bevy::{prelude::*, render::texture, transform::commands};
 
 pub struct OreLogicPlugin;
 
 impl Plugin for OreLogicPlugin {
     fn build(&self, app: &mut App) {
-    //app.add_systems(Startup, genesis);
+    app.add_systems(Startup, genesis);
     }
 }
 
+// Below was built using the 2d Sprite Sheets demo from assets
 
-fn genesis() {
+
+const SPRITESHEET_HEIGHT: usize = 640;
+const SPRITESHEET_WIDTH: usize = 640;
+const TEXTURE_PATH: &str = "terra/terrain.png";
+
+fn genesis(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load(TEXTURE_PATH);
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(SPRITESHEET_WIDTH as f32, SPRITESHEET_HEIGHT as f32), 7, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    // Use only the subset of sprites in the sheet that make up the run animation
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(1),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+
+    ));
 
 
 }
