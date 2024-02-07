f05f86245db2442c4d27d233e125c102afea6b58
UnemployedDad <anstrianese@gmail.com>
2024-02-07 13:48:16 -0700

    getting to your update

--- Industrial_Mage/src/main.rs
+++ Industrial_Mage/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Startup, setup1)
+        .add_systems(Startup, spawn_wizard)
         .add_systems(Update, sprite_movement)
         .run();
 }
@@ -48,31 +48,31 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("goblinhole/goblinwore.png"),
+            texture: asset_server.load("goblinhole/goblin.png"),
             transform: Transform::from_xyz(80., 0., 0.),
             ..default()
         },
         Direction::Up,
     ));
+
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("wiznerd/Wizard.png"),
-            transform: Transform::from_xyz(200., 0., 0.),
+            texture: asset_server.load("goblinhole/goblinwore.png"),
+            transform: Transform::from_xyz(380., 0., 0.),
             ..default()
         },
         Direction::Up,
     ));
 }
 
-fn setup1(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+fn spawn_wizard (mut commands: Commands, asset_server: Res<AssetServer>){
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("goblinhole/goblinwore.png"),
-            transform: Transform::from_xyz(-100., 0., 0.),
+            texture: asset_server.load("wiznerd/Wizard.png"),
+            transform: Transform::from_xyz(-380., 0., 0.),
             ..default()
         },
-        Direction::Up,
+        Direction::Down,
     ));
 }
 
@@ -84,7 +84,6 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
             Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
             Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
         }
-
         if transform.translation.y > 200. {
             *logo = Direction::Down;
         } else if transform.translation.y < -200. {
