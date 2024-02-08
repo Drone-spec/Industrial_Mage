e1985aa1239aebc783f5647211b9203998436763
UnemployedDad <anstrianese@gmail.com>
2024-02-08 09:45:30 -0700

    Our wizard now moves

--- Industrial_Mage/src/movement.rs
+++ Industrial_Mage/src/movement.rs
@@ -1,19 +1,54 @@
 
-use bevy::prelude::*;
+use bevy::{ecs::component, prelude::*};
+use noise::core::value;
 //Movement should all be handled here
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
-    app.add_systems(Update, update_position);
+    app.add_systems(Update, (update_position, update_velocity));
     }
 }
-
 
 #[derive(Component, Debug)]
 pub struct Velocity {
     pub value: Vec3,
 }
+
+impl Velocity 
+{
+    pub fn new(value: Vec3) -> Self {Self {value}}
+}
+    
+
+#[derive(Component, Debug)]
+pub struct Acceleration
+{
+    pub value: Vec3,
+}
+
+impl Acceleration
+{
+    pub fn new(value: Vec3) -> Self {Self {value}}
+}
+
+#[derive(Bundle)]
+
+pub struct MovingObjectBundle
+{
+   pub velocity: Velocity,
+   pub acceleration: Acceleration,
+   pub model: SpriteBundle,
+}
+
+fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>, time: Res<Time>) 
+{
+    for (acceleration, mut velocity) in query.iter_mut()
+    {
+        velocity.value += acceleration.value * time.delta_seconds();
+    }
+}
+
 
 pub fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>)
 {
--- Industrial_Mage/src/wizard.rs
+++ Industrial_Mage/src/wizard.rs
@@ -1,26 +1,64 @@
-use bevy::prelude::*;
-use crate::movement::*;
+use std::arch::x86_64::_MM_FLUSH_ZERO_OFF;
+
+use bevy::{math::vec3, prelude::*, transform, ui::update};
+use crate::movement::{self, *};
+
+const STARTING_TRANSLATION: Vec3 = Vec3::new(500., 0., 0.);
+const WIZARD_SPEED: f32 = 250.0;
 
 // All Wizard stuff should be in here..
 pub struct WizardPlugin;
+
+#[derive(Component, Debug)]
+pub struct Wizard;
+
 impl Plugin for WizardPlugin {
     fn build(&self, app: &mut App) {
-    app.add_systems(Startup, spawn_wizard);
+    app.add_systems(Startup, spawn_wizard)
+        .add_systems(Update, wizard_movement_controls);
     }
 }
 
-
-pub fn spawn_wizard(mut commands: Commands, asset_server: Res<AssetServer> ) {
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("wiznerd/Wizard.png"),
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
+pub fn spawn_wizard(mut commands: Commands, asset_server: Res<AssetServer> )
+ {
+    commands.spawn((MovingObjectBundle 
+        {
+            velocity: Velocity::new(Vec3::ZERO),
+            acceleration: Acceleration::new(Vec3::ZERO),
+            model: SpriteBundle
+            {
+                texture: asset_server.load("wiznerd/Wizard.png"),
+                transform: Transform::from_translation(STARTING_TRANSLATION),
+                ..default()
+            },
         },
-        Velocity {
-            value: Vec3::new(0., 0., 0.)
-        }
+    Wizard,
     ));
+}
 
+fn wizard_movement_controls(mut query: Query<(&mut Transform, &mut Velocity), With<Wizard>>, keyboard_input: Res<Input<KeyCode>>) 
+{
+    let(transform, mut velocity) = query.single_mut();
+
+        if keyboard_input.pressed(KeyCode::S)
+    {
+        velocity.value = transform.down() * WIZARD_SPEED;
+    } 
+    else if keyboard_input.pressed(KeyCode::W)
+    {
+        velocity.value = transform.up() * WIZARD_SPEED;
+    }
+    else if keyboard_input.pressed(KeyCode::A)
+    {
+        velocity.value = transform.left() * WIZARD_SPEED;
+    } 
+    else if keyboard_input.pressed(KeyCode::D)
+    {
+        velocity.value = transform.right() * WIZARD_SPEED;
+    }
+    else
+    {
+        velocity.value = transform.forward();
+    }
 
 }
