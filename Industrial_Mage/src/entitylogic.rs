ed3a5a1618842db9a7a5e34754f4ae9d54c5d67e
drone-spec <drone@dronemothership.work>
2024-02-09 16:39:27 -0600

    I have the Mana and Health Systems working. I need to iron out how to have it go in a better way but for now its all running.

--- Industrial_Mage/src/entitylogic.rs
+++ Industrial_Mage/src/entitylogic.rs
@@ -7,10 +7,16 @@ use crate::wizard::*;
 use crate::movement::{Acceleration, MovingObjectBundle, Velocity};
 pub struct EntityLogic;
 
+#[derive(Resource)]
+pub struct RegenTimer(Timer);
+
 impl Plugin for EntityLogic {
     fn build(&self, app: &mut App) {
-    app.add_systems(Startup, spawn_goblin)
-    .add_systems(Update, (health_regen, mana_regen));
+    app.insert_resource(RegenTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+    .add_systems(Startup, spawn_goblin)
+    .add_systems(Update, health_regen)
+    .add_systems(Update, mana_regen)
+    ;
     }
 }
 
@@ -43,31 +49,69 @@ struct BasicGoblin {
     mana: Mana,
     movingobjbun: MovingObjectBundle,
 }
-fn health_regen(mut query: Query<(Entity, &mut Health)>, time: Res<Time>){
-    
-    for (Entity, mut Health) in query.iter_mut()
-    {
-        if Health.regen == true {
-            Health.amount += Health.regenamount * time.delta_seconds ();
-            info!("Entity: {:?} is at has {:?} Health!", Entity, Health.amount);
-        } 
-        
-    }
 
+
+/// The Health and Mana system work the exact same!!
+/// It just Query's for every Entity with Health and the pull Time and the Resource time.
+/// If Resource time has passed its value then run the below System That system will see if Regen is enabled and then ask the entity
+/// what its regen amount is and apply that to the main amount for that entity.
+fn health_regen(mut query: Query<(Entity, &mut Health)>, time: Res<Time>, mut timer: ResMut<RegenTimer>){
+    if timer.0.tick(time.delta()).just_finished() {
+        for (Entity, mut Health) in query.iter_mut()
+        {   
+            match Health.regen {
+                true =>
+                if Health.max > Health.amount {
+                    Health.amount += Health.regenamount + (time.delta_seconds() * 2.0);
+                    info!("Entity: {:?} is at {:?} Health!", Entity, Health.amount);
+                }
+                else 
+                {
+                    info!("Entity: {:?} is at has max Health! {:?}", Entity, Health.amount);
+                },
+                false => 
+                if Health.max > Health.amount {
+                    info!("Entity: {:?} is at {:?} Health and Regen is hindered!", Entity, Health.amount);
+                }   
+                else
+                {
+                    info!("Entity: {:?} is at {:?} Health! Regen is hindered", Entity, Health.amount);
+                },
+                _ => info!("How did you get here? Error at True False in health_regen") 
+            }
+        }
+    }
 }
 
 #[allow(dead_code)]
-fn mana_regen(mut query: Query<(Entity, &mut Mana)>, time: Res<Time>) {
-    
-    for (Entity, mut Mana) in query.iter_mut()
-    {
-        if Mana.regen == true {
-            Mana.amount += Mana.regenamount * time.delta_seconds ();
-            info!("Entity: {:?} is at has {:?} Mana!", Entity, Mana.amount);
-        } 
-        
+fn mana_regen(mut query: Query<(Entity, &mut Mana)>, time: Res<Time>, mut timer:ResMut<RegenTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (Entity, mut Mana) in query.iter_mut()
+        {
+            match Mana.regen {
+                true =>
+                if Mana.max > Mana.amount {
+                    Mana.amount += Mana.regenamount + (time.delta_seconds() * 2.0);
+                    info!("Entity: {:?} is at {:?} Mana!", Entity, Mana.amount);
+                }
+                else 
+                {
+                    info!("Entity: {:?} is at has max Mana! {:?}", Entity, Mana.amount);
+                },
+                false => 
+                if Mana.max > Mana.amount {
+                    info!("Entity: {:?} is at {:?} Mana and Regen is hindered!", Entity, Mana.amount);
+                }   
+                else
+                {
+                    info!("Entity: {:?} is at {:?} Mana! Regen is hindered", Entity, Mana.amount);
+                },
+                _ => info!("How did you get here? Error at True False in Mana_regen") 
+            }
+        }
     }
-} 
+}
+ 
 
 
 
@@ -78,7 +122,32 @@ fn spawn_goblin(mut commands: Commands, asset_server: Res<AssetServer> ){
     commands.spawn((BasicGoblin 
         {
             health: Health{
-                amount: 100.0,
+                amount: 50.0,
+                max: 100.0,
+                regen: true,
+                regenamount: 1.0
+            },
+            mana: Mana{
+                amount: 0.0,
+                max: 100.0,
+                regen: true,
+                regenamount: 1.0
+            },
+            movingobjbun: MovingObjectBundle {
+                velocity: Velocity::new(Vec3::ZERO),
+                acceleration: Acceleration::new(Vec3::ZERO),
+                model: SpriteBundle{
+                    texture: asset_server.load("goblinhole/goblin.png"),
+                    transform: Transform::from_translation(STARTING_TRANSLATION),
+                    ..default()
+                }
+            },
+        },
+    ));
+    commands.spawn((BasicGoblin 
+        {
+            health: Health{
+                amount: 50.0,
                 max: 100.0,
                 regen: true,
                 regenamount: 1.0
